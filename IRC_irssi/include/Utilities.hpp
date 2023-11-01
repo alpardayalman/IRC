d08022664772674440e0a117fe2446577b20b236
Arda Yalman <master@192.168.1.103>
2023-11-02 00:34:21 +0300

    trimliyoz

--- IRC_irssi/include/Utilities.hpp
+++ IRC_irssi/include/Utilities.hpp
@@ -5,6 +5,10 @@ class Utilities {
     private:
         Utilities() {}
         ~Utilities() {}
+        static std::string ltrim(const std::string &s);
+        static std::string rtrim(const std::string &s);
+
     public:
         static int checkPortNumber(char *port_number);
+        static std::string trim(const std::string &s);
 };
