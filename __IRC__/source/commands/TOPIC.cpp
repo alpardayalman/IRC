eb04b67b783776f21fe39991088ea482eec4a931
Emircan Kaymaz <ekaymaz@k2m09s06.42kocaeli.com.tr>
2023-11-06 20:30:55 +0300

    We hava a new command Her name is Topic

---
+++ __IRC__/source/commands/TOPIC.cpp
@@ -0,0 +1,18 @@
+#include "Server.hpp"
+
+int     Server::Topic(std::string &s, Client &cli) {
+    std::cout << "\ntopic: " << s << std::endl;
+    std::stringstream ss(s);
+    std::string str, cha;
+    ss >> cha >> str;
+    Utilities::trim(cha);
+    if(cha == "ft_irc") {
+        write(cli.cliFd, "You cannot change ft_irc", 25);
+    }
+    else {
+        std::string sendi = RPL_TOPIC(cli.nick, cli.ipAddr, cha, &str[1]);
+        write(cli.cliFd, sendi.c_str(), sendi.size());
+    }
+
+    return(1);
+}
