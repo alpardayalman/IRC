ed01c2ac41cfffaf1fc5148de6d0fa8c05b29d51
Talha Acikgoz <tacikgoz58@gmail.com>
2023-11-08 16:24:26 +0300

    just ping ok?

--- __IRC__/source/commands/PING.cpp
+++ __IRC__/source/commands/PING.cpp
@@ -1,7 +1,15 @@
 #include "Server.hpp"
 
+            if (params[1].find("PING")) {
+                std::string s = params[1].substr(5, params[1].length());
+                Server::Ping(s, (*it));
+                break;
+            }
+
 int     Server::Ping(std::string &s, Client &cli) {
-    std::string png = ":" + cli.getPrefix() + RPL_PONG(s);
-    write(this->server_fd, png.c_str(), png.length());
+    std::vector<std::string> param = Utilities::tokenCmd(s, 1);
+    std::cout << "param0" << param[0] << " s:" << s << std::endl;
+    std::string png = RPL_PING(param[0], cli.nick+" :" + param[0]);
+    write(cli.cliFd, png.c_str(), png.length());
     return 1;
 }
