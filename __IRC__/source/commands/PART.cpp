a306197b9c4c1f58af59b74c7a8a2f5bcb1d13da
Emircan Kaymaz <ekaymaz@k2m10s03.42kocaeli.com.tr>
2023-11-11 16:42:59 +0300

    PART final form

--- __IRC__/source/commands/PART.cpp
+++ __IRC__/source/commands/PART.cpp
@@ -11,33 +11,36 @@ int Server::Part(std::string &s, Client &cli) {
     std::string msg; // yanlarda ad silinsin diye.
     ChanelIterator it = this->chanels.begin();
     for (; it != this->chanels.end(); ++it) {
-        if (it->name == s){
-            it->op = &it->clients[0];//uga buga adresler degismek iteratorda
-            for (ClientIterator cit = it->clients.begin(); cit != it->clients.end(); ++cit) {
+        if (it->name == s){ // Chanel bulunduysa
+            for (ClientIterator cit = it->clients.begin(); cit != it->clients.end(); ++cit) { // chaneldeki clientlari gez
                 if (cit->nick == cli.nick) {
-                    if (it->op->nick == cit->nick) {
+                    if (it->op->nick == cit->nick) { // op ise
                         if (it->clients.size() > 1) {
-                            ;
+                            it->op = &it->clients[0];//uga buga adresler degismek iteratorda
                         }
-                    }
-                    std::cout << &it->clients[0].nick << std::endl;
+                        else {
+                            this->chanels.erase(it);
+                            it->op = NULL;
+                        }
+                    }    
                     it->clients.erase(cit);
-                    std::cout << &it->clients[0].nick << std::endl;
+                }
+                if (cit == it->clients.end())
                     break;
-                }
-                else {
-                    msg += (*cit).user + " "; // operator eklenilecek.
-                }
             }
-            if (!it->clients.size()) {
-                this->chanels.erase(it);
+            for (ClientIterator cit = it->clients.begin(); cit != it->clients.end(); ++cit) {
+                    if(it->op->nick == cit->nick)
+                        msg += "@";
+                    msg += (*cit).user + " ";
             }
+                std::cout << RPL_NAMREPLY(cli.nick, it->name, msg) << std::endl;
+                Utilities::writeAllRpl(this->getFds(), RPL_NAMREPLY(cli.nick, it->name, msg));
+                Utilities::writeAllRpl(this->getFds(), RPL_ENDOFNAMES(cli.nick, it->name));
             break;
         }
     }
-    cli.messageBox.push_back(RPL_PART(cli.getPrefix(), s));
-    FD_SET(cli.cliFd, &this->writeFds);
-    Utilities::writeAllRpl(this->getFds(), RPL_NAMREPLY(cli.nick, it->name, msg));
-    Utilities::writeAllRpl(this->getFds(), RPL_ENDOFNAMES(cli.nick, it->name));
+    std::cout << "chanel size: " << it->clients.size() << std::endl;
+    Utilities::writeRpl(cli.cliFd, RPL_PART(cli.getPrefix(), s));
+
     return 1;
 }
