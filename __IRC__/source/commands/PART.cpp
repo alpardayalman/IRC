ce65a2c2730e6d1ac730fa3ee66973f75c925936
Alp arda Yalman <ayalman@k2m10s04.42kocaeli.com.tr>
2023-11-13 00:11:36 +0300

    [PART] deleting comments

--- __IRC__/source/commands/PART.cpp
+++ __IRC__/source/commands/PART.cpp
@@ -1,22 +1,18 @@
 #include "Server.hpp"
 
-
-
-// server -> chanel -> user silinecek.
-// /kick #test1 ayalman why not
 int Server::Part(std::string &s, Client &cli) {
     std::stringstream ss(s);
     ss >> s;
 
-    std::string msg; // yanlarda ad silinsin diye.
+    std::string msg;
     ChanelIterator it = this->chanels.begin();
     for (; it != this->chanels.end(); ++it) {
-        if (it->name == s){ // Chanel bulunduysa
-            for (ClientIterator cit = it->clients.begin(); cit != it->clients.end(); ++cit) { // chaneldeki clientlari gez
+        if (it->name == s){
+            for (ClientIterator cit = it->clients.begin(); cit != it->clients.end(); ++cit) {
                 if (cit->nick == cli.nick) {
-                    if (it->op->nick == cit->nick) { // op ise
+                    if (it->op->nick == cit->nick) {
                         if (it->clients.size() > 1) {
-                            it->op = &it->clients[0];//uga buga adresler degismek iteratorda
+                            it->op = &it->clients[0];
                         }
                         else {
                             it->clients.erase(cit);
