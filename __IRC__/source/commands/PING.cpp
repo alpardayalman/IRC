085893d87f335be70e9a72d777060fa585111f03
Talha Acikgoz <tacikgoz58@gmail.com>
2023-11-13 00:40:38 +0300

    [PING] cout's cleaned

--- __IRC__/source/commands/PING.cpp
+++ __IRC__/source/commands/PING.cpp
@@ -16,12 +16,10 @@ int     Server::Ping(std::string &s, Client &cli) {
         param = Utilities::splitString(s);
         if (s.at(0) == '0' && s.at(1) == '0') {
             param[0] = param[0].substr(2, param[0].length());
-            std::cout << YELLOW << "aloooo:" << param[0] << " ss::" << s << RESET << std::endl;
             Utilities::writeRpl(cli.cliFd, RPL_PING(cli.getPrefix(), param[0], s.substr(s.find("PING")+5, s.length()-1)));
         } else if (s.find(":PING") && param[1].size()) {//if PRIVMSG ile gelirse buraya girecek// PRIVMSG <target nick> :PING <komutun yollandigi saniye>//rpl_ping ile yollarsaniz gonderilen saniyeden o anki saniyeyi cikartip cliente yazdiriyor.
             Client target = getCli(this->clients, param[0]);
 
-            std::cout << PURPLE << "alooo222:" << s << RESET << std::endl;
             Utilities::writeRpl(cli.cliFd, RPL_PING(cli.getPrefix(), param[0], s.substr(s.find("PING")+5, s.length()-1)));
             std::string msg = "00" + cli.nick + " :PING " + param[2];
             Server::Ping(msg, target);
