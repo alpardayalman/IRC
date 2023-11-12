38fbd8804a247cda07978e875fb7a8a792d83f90
Alp arda Yalman <ayalman@k2m10s04.42kocaeli.com.tr>
2023-11-13 00:13:23 +0300

    [SERVER] leaks check added

--- __IRC__/source/main.cpp
+++ __IRC__/source/main.cpp
@@ -3,6 +3,16 @@
 #include "Exception.hpp"
 #include <stdlib.h>
 #include <stdexcept>
+#include <iostream>
+
+#define SYSTEM
+
+void    signalHandle(int signum) {
+#ifdef SYSTEM
+    system("leaks ircserv"); //
+#endif
+    std::exit(signum);
+}
 
 int main(int ac, char **av) {
     try {
@@ -10,8 +20,10 @@ int main(int ac, char **av) {
             throw Exception("Give arguments like the following ./exec <port> <password>.");
         else if (!Utilities::checkPortNumber(av[1]))
             throw Exception("Port has to be all digits.");
-        else
+        else {
+            std::signal(SIGINT, signalHandle);
             Server a = Server(atoi(av[1]), av[2]);
+        }
     }
     catch (std::exception &ex) {
         std::cerr << ex.what() << std::endl;
