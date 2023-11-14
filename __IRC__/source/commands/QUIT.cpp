05795caf062385fdb93e56ca8726a690d2261ec0
Emircan Kaymaz <ekaymaz@k2m09s03.42kocaeli.com.tr>
2023-11-14 18:17:50 +0300

    [quit] son dk fast push

--- __IRC__/source/commands/QUIT.cpp
+++ __IRC__/source/commands/QUIT.cpp
@@ -5,6 +5,13 @@ int Server::Quit(std::string &s, Client &cli) {
     for (ChanelIterator it = this->chanels.begin(); it != this->chanels.end(); ++it) {
         Server::Part(it->name, cli);
 	}
+    for (ClientIterator it = this->clients.begin(); it != this->clients.end(); ++it) {
+        if (it->nick == cli.nick) {
+            std::cout << YELLOW << "Client " << cli.nick << " has left the server." << RESET << std::endl;
+            this->clients.erase(it);
+            break;
+        }
+    }
     Utilities::writeRpl(cli.cliFd, RPL_QUIT(cli.getPrefix(), s.c_str()));
     close(cli.cliFd);
     return 1;
