b4151714e8118d9e1a663e9e27ee09e461357cd2
Emircan Kaymaz <ekaymaz@k2m09s03.42kocaeli.com.tr>
2023-11-14 16:11:51 +0300

    [PRIVMSG] nc and hexchat message : problem solved

--- __IRC__/source/commands/PRIVMSG.cpp
+++ __IRC__/source/commands/PRIVMSG.cpp
@@ -7,22 +7,22 @@ int     Server::PrivMsg(std::string &s, Client& cli) {
     std::vector<std::string> params = Utilities::tokenCmd(s, 0); // cmd[0] eger chanel ise basinda # olucak client ise olmayacak.
     if (!cli.passcheku)
         return 0;
-    if (!isClientIn(cli, params[0]))
-        return 1;
-    for(std::vector<Client>::iterator it = this->clients.begin(); it != this->clients.end();++it)
+    for(std::vector<Client>::iterator it = this->clients.begin(); it != this->clients.end();++it) // tum clientlarda geziyoruz.
     {
-        if (it->cliFd != cli.cliFd && isClientIn((*it), params[0]))//chanel chat
+        if (it->cliFd != cli.cliFd && isClientIn((*it), params[0]))// eger kendin degil isen ve client(it) bu chanelde ise
         {
-            (*it).messageBox.push_back(RPL_PRIVMSG(cli.getPrefix(), params[0], params[1].substr(1, params[1].length())));
+            if (!isClientIn(cli, params[0])) // sen chanelde misin.
+                return 1;
+            (*it).messageBox.push_back(RPL_PRIVMSG(cli.getPrefix(), params[0], (params[1][0]==':'?params[1].substr(1, params[1].length()) : params[1])));
             FD_SET((*it).cliFd, &this->writeFds);
         }
         else if (params[0] == (*it).user || params[0] == (*it).nick) {//PRIVMSG <user> <msg> PRIVMSG <nick> <msg>
-            if (params[1].find("PING") != std::string::npos) {
+            if (params[1].find("PING") != (unsigned long) -1) {
                 s = s + " " +cli.nick;
                 Server::Ping(s, cli);
                 break;
             }
-            (*it).messageBox.push_back(RPL_PRIVMSG(cli.getPrefix(), (*it).nick, params[1]));
+            (*it).messageBox.push_back(RPL_PRIVMSG(cli.getPrefix(), (*it).nick, (params[1][0]==':'?params[1].substr(1, params[1].length()) : params[1])));
             FD_SET((*it).cliFd, &this->writeFds);
         }
     }
