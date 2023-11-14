f88f0944d398a4c34d9b6f294f2d7bd901593461
Talha Acikgoz <tacikgoz58@gmail.com>
2023-11-14 16:51:47 +0300

    [OP] op is fixed.

--- __IRC__/source/commands/OP.cpp
+++ __IRC__/source/commands/OP.cpp
@@ -1,5 +1,4 @@
 #include "Server.hpp"
-//ha buraya namerpl_
 
 void showUsers(Chanel &cha) {
     std::cout << PURPLE;
@@ -26,11 +25,20 @@ int Server::Op(std::string &s, Client &cli) {//  s = <chanelname> +o <nickname>
     Chanel cha = getChanel(cmd[0]);
     if (cli.nick == cha.op->nick) {//komutu gonderen client eger op ise
         Utilities::trim(cmd[2]);
+        ClientIterator it_ = this->clients.begin();
+        for (; it_ != this->clients.end(); ++it_) {
+            if (it_->nick == cmd[2])
+                break;
+        }
+        if (it_ == this->clients.end())
+            return 1;
         Client newOp = cha.getClient(cmd[2]);
         Client oldOp = cha.getClient(cli.nick);
+
+        std::cout << YELLOW << "cmd:" << cmd[2] << " s:" << s << RESET << std::endl;
         if (isClientIn(oldOp, cmd[0]) && isClientIn(newOp, cmd[0])) {//eger oldOperator ve newOperator ayni kanalda ise
             for (ChanelIterator it = chanels.begin(); it != chanels.end(); it++) {
-                if (cmd[0] == it->name) {
+                if (cmd[0] == it->name && getClientPos(*it, oldOp) != -1 && getClientPos(*it, newOp) != -1) {
                     //inputtan gelen chanel name iteratorun chanelNameine esitse new ve olp op'u swapliyor
                     Client tmp = it->clients[getClientPos(*it, oldOp)];
                     it->clients[getClientPos(*it, oldOp)] = it->clients[getClientPos(*it, newOp)];
@@ -39,17 +47,9 @@ int Server::Op(std::string &s, Client &cli) {//  s = <chanelname> +o <nickname>
                 }
             }
         }
-        std::string msg;
-        for(std::vector<Client>::iterator it = cha.clients.begin() ; it != cha.clients.end(); ++it) {
-                if(int chidx = isClientIn((*it), cmd[0])) {
-                    if (it->cliFd == this->chanels[chidx-1].op->cliFd)
-                        msg += "@";
-                    msg += (*it).nick + " ";
-                }
-            }
-        Utilities::writeAllRpl(this->getFds(), RPL_NAMREPLY(cli.nick, cmd[0], msg));
-        Utilities::writeAllRpl(this->getFds(), RPL_ENDOFNAMES(cli.nick, cmd[0]));
+        Server::showRightGui(cli, cha);
     } else {//find(rpl_erroryournotoperator())
+        Utilities::writeRpl(cli.cliFd, ERR_CHANOPRIVSNEEDED(cli.getPrefix(), cha.name));
     }
     return 1;
 }
