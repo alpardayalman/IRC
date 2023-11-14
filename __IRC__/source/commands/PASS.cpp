d416505933995d4b4932cbe356febd20a42e7c97
yeaktas <yunusemreaktas97@gmail.com>
2023-11-14 15:59:14 +0300

    [C++98 - PASS] write return control added, to_string deleted

--- __IRC__/source/commands/PASS.cpp
+++ __IRC__/source/commands/PASS.cpp
@@ -1,13 +1,16 @@
 #include "Server.hpp"
 #include "Utilities.hpp"
+#include "Exception.hpp"
 
 int     Server::Pass(std::string &s, Client& cli) {  // pass hatali rpli var onu bul ve entegre et
     if (cli.passcheku)
         return 1;
     else if  (this->checkPassword(s, cli)) {
         cli.passcheku = 1;
-        write(cli.cliFd, "Password is correct\n", 20);
-        std::cout << GREEN << "Client: " + std::to_string(cli.cliFd -3) + " Pass: " + cli.pass + " has the password correctly\n" << WHITE << std::endl;
+        int result = write(cli.cliFd, "Password is correct\n", 20);
+        if(result < 0)
+            throw Exception("Write password error!");
+        std::cout << GREEN << "Client: " << (cli.cliFd -3) << " Pass: " + cli.pass + " has the password correctly\n" << WHITE << std::endl;
         
         return 1;
     }
