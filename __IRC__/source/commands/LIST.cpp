4a5dfcb667189267350fd606484b816158b1338d
Emircan Kaymaz <99013427+EmirKymz@users.noreply.github.com>
2023-11-14 16:30:43 +0300

    [C++98 - LIST] intToString
    static_cast >> intToString

--- __IRC__/source/commands/LIST.cpp
+++ __IRC__/source/commands/LIST.cpp
@@ -2,12 +2,12 @@
 
 int    Server::List(std::string &s, Client& cli) {
     (void) s;
-    Utilities::writeRpl(cli.cliFd, RPL_LISTSTART(cli.nick, static_cast<char>(this->clients.size())));
+    Utilities::writeRpl(cli.cliFd, RPL_LISTSTART(cli.nick, Utilities::intToString(this->clients.size())));
     for (ChanelIterator it = this->chanels.begin(); it != this->chanels.end(); ++it) {
         if (!it->topic.length())
-            Utilities::writeRpl(cli.cliFd, RPL_LIST(cli.nick, it->name, static_cast<char>(it->clients.size()), " Empty"));
+            Utilities::writeRpl(cli.cliFd, RPL_LIST(cli.nick, it->name, Utilities::intToString(it->clients.size()), " Empty"));
         else
-            Utilities::writeRpl(cli.cliFd, RPL_LIST(cli.nick, it->name, static_cast<char>(it->clients.size()), it->topic));
+            Utilities::writeRpl(cli.cliFd, RPL_LIST(cli.nick, it->name, Utilities::intToString(it->clients.size()), it->topic));
     }
     Utilities::writeRpl(cli.cliFd, RPL_LISTEND(cli.nick));
     return 1;
