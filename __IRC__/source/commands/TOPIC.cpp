94ef46cddf45f66a1cf3549c726c17ddbe4c4be2
Emircan Kaymaz <ekaymaz@k2m09s03.42kocaeli.com.tr>
2023-11-14 16:12:40 +0300

    [TOPIC] nc and hexchat message : problem solved

--- __IRC__/source/commands/TOPIC.cpp
+++ __IRC__/source/commands/TOPIC.cpp
@@ -2,18 +2,22 @@
 
 int     Server::Topic(std::string &s, Client &cli) {
 
-    std::stringstream ss(s);
-    std::string str, cha;
-    ss >> cha;
-    str = s.erase(0, cha.length());
-    Utilities::trim(cha);
+    // std::stringstream ss(s);
+    // std::string str, cha;
+    // ss >> cha;
+    // str = s.erase(0, cha.length());
+    std::vector<std::string> params;
+    // Utilities::trim(cha);
+    // Utilities::tokemCmd(str,)
+    params = Utilities::tokenCmd(s, 1);
+
     for (ChanelIterator it = this->chanels.begin(); it != this->chanels.end(); ++it) {
-        if (it->name == cha) {
-            if(!str[1])
-                Utilities::writeRpl(cli.cliFd, RPL_NOTOPIC(cli.nick, cha));
+        if (it->name == params[0]) {
+            if(params[1].empty())
+                Utilities::writeRpl(cli.cliFd, RPL_NOTOPIC(cli.nick, params[0]));
             else
-                Utilities::writeAllRpl(this->getFds(), RPL_TOPIC(cli.nick, cli.ipAddr, cha, &str[1]));
-            it->topic = &str[1];
+                Utilities::writeAllRpl(this->getFds(), RPL_TOPIC(cli.nick, cli.ipAddr, params[0], (params[1][0]==':'?params[1].substr(1, params[1].length()):params[1])));
+            it->topic = (params[1][0]==':'?params[1].substr(1, params[1].length()):params[1]);
             break;
         }
     }
